e4c7a63315f3b9aaa450dfed62eda32e40d04c12
AlpacaRex <1030519668@qq.com>
2022-12-12 22:41:47 +0800

    search

--- Call_Me_of_Academics/settings.py
+++ Call_Me_of_Academics/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'CMA',
         'USER': 'root',
-        'PASSWORD': 'jyy13575305668',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
